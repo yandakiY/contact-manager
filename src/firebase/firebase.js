bdeb8d60cc3a27551e6d15be95881d20ff7571da
yandaki <boayves@gmail.com>
2023-05-14 13:18:05 +0000

    Change config 2

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLm7zs0DvW2Vy6wXHTBkeCHptBeTGLqqU",
-  authDomain: "contacts-manager-7f2c4.firebaseapp.com",
-  projectId: "contacts-manager-7f2c4",
-  storageBucket: "contacts-manager-7f2c4.appspot.com",
-  messagingSenderId: "500132213033",
-  appId: "1:500132213033:web:ead0dab467ea00302b8738",
-  measurementId: "G-304SMMY40W"
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
+  projectId: process.env.REACT_PROJECT_ID,
+  storageBucket: process.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
