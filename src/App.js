dd88bd405961c078e8b4edb6d8e310dc78e0b231
yandaki <boayves@gmail.com>
2023-05-14 13:04:40 +0000

    Add configuration and packages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  console.log(process.env.REACT_APP_API_KEY)
   return (
     <div className="App">
       <header className="App-header">
